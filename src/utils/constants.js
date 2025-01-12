98143a3db0eeb2a8d88ff46df752eb695360efcd
Vijay07vj <vijay0710vj@gmail.com>
2025-01-12 16:43:23 +0530

    Latest Changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,12 +2,6 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const RES_URL =
-  "https://corsproxy.org/?" +
-  encodeURIComponent(
-    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId="
-  );
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId=";
 export const APP_URL =
-  "https://corsproxy.org/?" +
-  encodeURIComponent(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  );
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
